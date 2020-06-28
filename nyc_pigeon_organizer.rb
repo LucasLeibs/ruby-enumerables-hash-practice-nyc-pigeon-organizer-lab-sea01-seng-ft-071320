3d287652a56c04f7f9a8c5f35eed2d6beb94c711
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-06-28 16:56:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,5 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attributes, attributes_values|
-    attributes_values.each do |
+    attributes_values.each do |names, descrip
 end
